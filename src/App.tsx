fb2e20bd2933080360a848016c0d3c4120ad0d81
Kenanimok <touch@sutha-MacBook-Pro.local>
2022-09-23 11:25:17 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes,Navigate} from 'react-router'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
 
-function App() {
+type Props = {}
+
+export default function App({}: Props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Routes>
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/" element={<Navigate to="/login" />} />
 
-export default App;
+    </Routes>
+    </>
+    )
+}
---
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function RegisterPage({}: Props): ReactElement {
+    return (
+        <div>
+ccc
+        </div>
+    )
+}
