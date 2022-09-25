6594cc1daf4e52147bf3d0d98b856f1075ba0d4c
Kenanimok <touch@sutha-MacBook-Pro.local>
2022-09-25 15:46:52 +0700

    fix add type user regis/login

--- src/components/pages/RegisterPage/RegisterPage.tsx
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,18 @@
-import React, { ReactElement } from "react";
-import { useState } from "react";
-import { Formik, FormikProps } from "formik";
+import * as React from "react";
 import { useNavigate } from "react-router-dom";
+import { Formik, FormikProps } from "formik";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Stack,
+  SxProps,
+  TextField,
+  Theme,
+  Typography,
+} from "@mui/material";
+import { User } from "../../../types/user.type";
 
 type RegisterPageProps = {
   //
@@ -9,56 +20,128 @@ type RegisterPageProps = {
 
 const RegisterPage: React.FC<any> = () => {
   const navigate = useNavigate();
+  const classes: SxProps<Theme> | any = {
+    root: { display: "flex", justifyContent: "center" },
+    buttons: { marginTop: 2 },
+  };
 
-  const showForm = ({
+  // const showFormV1 = ({
+  //   handleSubmit,
+  //   handleChange,
+  //   isSubmitting,
+  //   values,
+  // }: FormikProps<any>) => {
+  //   return (
+  //     <form onSubmit={handleSubmit}>
+  //       <label>Username: </label>
+  //       <input
+  //         type="text"
+  //         name="username"
+  //         id="username"
+  //         onChange={handleChange}
+  //         value={values.username}
+  //       />
+  //       <br />
+  //       <label>Password: </label>
+  //       <input
+  //         type="text"
+  //         name="password"
+  //         id="password"
+  //         onChange={handleChange}
+  //         value={values.password}
+  //       />
+  //       <br />
+
+  //       <button type="submit" disabled={isSubmitting}>
+  //         Submit
+  //       </button>
+  //       <button onClick={() => navigate(-1)}>Back</button>
+  //     </form>
+  //   );
+  // };
+
+  const showFormV2 = ({
     handleSubmit,
     handleChange,
     isSubmitting,
     values,
-  }: FormikProps<any>) => {
+  }: FormikProps<User>) => {
     return (
       <form onSubmit={handleSubmit}>
-        <label>Username: </label>
-        <input
-          type="text"
-          name="username"
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
           id="username"
+          label="Username"
           onChange={handleChange}
           value={values.username}
+          autoComplete="email"
+          autoFocus
         />
-        <br />
-        <label>Password: </label>
-        <input
-          type="text"
-          name="password"
+
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
           id="password"
+          label="Password"
           onChange={handleChange}
           value={values.password}
+          type="password"
         />
         <br />
 
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-        <button onClick={() => navigate(-1)}>Back</button>
+        <Stack direction="row" spacing={2} sx={classes.buttons}>
+          <Button
+            onClick={() => navigate("/login")}
+            type="button"
+            fullWidth
+            variant="outlined"
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+          >
+            Create
+          </Button>
+        </Stack>
       </form>
     );
   };
 
+  const initialValues: User = { username: "", password: "" };
+
   return (
     <>
-      <Formik
-        onSubmit={(values, { setSubmitting }) => {
-          alert(JSON.stringify(values));
+      <Box sx={classes.root}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Register
+            </Typography>
+            <Formik
+              onSubmit={(values, { setSubmitting }) => {
+                alert(JSON.stringify(values));
 
-          setTimeout(() => {
-            setSubmitting(false);
-          }, 2000);
-        }}
-        initialValues={{ username: "lek", password: "xxxx" }}
-      >
-        {(props) => showForm(props)}
-      </Formik>
+                setTimeout(() => {
+                  setSubmitting(false);
+                }, 2000);
+              }}
+              initialValues={initialValues}
+            >
+              {(props) => showFormV2(props)}
+            </Formik>
+          </CardContent>
+        </Card>
+      </Box>
     </>
   );
 };
